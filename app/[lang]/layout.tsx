acaf5488ea78c7583ccf4f5c27b99d18edcf798f
0xZales <rozalesamiral@gmail.com>
2023-05-21 22:26:45 +0000

    added vercel Analytics

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -14,6 +14,7 @@ import { getDictionary } from '@/lib/get-dictionary'
 import { Locale } from '@/config/i18n-config'
 import { Dictionary } from '@/types'
 import Head from 'next/head'
+import { Analytics } from '@/components/analytics'
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
@@ -108,6 +109,7 @@ export default async function RootLayout({
           {/* <Toaster /> */}
           {/* </ThemeProvider> */}
           <SiteFooter dictionary={dictionary.footer} />
+          <Analytics />
         </Providers>
 
 
