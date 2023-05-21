39c20e2d583aba027a40fd43ef4754d19da40f7f
0xZales <rozalesamiral@gmail.com>
2023-05-21 19:34:29 +0000

    fff

--- hooks/use-lock-body.ts
+++ hooks/use-lock-body.ts
@@ -2,7 +2,7 @@ import * as React from "react"
 
 // @see https://usehooks.com/useLockBodyScroll.
 export function useLockBody() {
-  React.useLayoutEffect((): (() => void) => {
+  React.useLayoutEffect((): (()  => void) => {
     const originalStyle: string = window.getComputedStyle(
       document.body
     ).overflow
